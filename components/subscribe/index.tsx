a3898e9523609e748f08b0a6faab37cb6e65a6de
Tyler Ruff <hello@blazed.space>
2023-09-27 22:22:56 -0400

    Update index.tsx

--- components/subscribe/index.tsx
+++ components/subscribe/index.tsx
@@ -16,7 +16,7 @@ export default function Subscribe(){
     return (
         <article aria-label="Subscirbe Form" style={{backgroundImage: 'url(/images/damaged-glass.jpg'}} className="hero select-none">
             <div className="hero-overlay bg-opacity-20"></div>
-            <div className="hero mx-auto max-w-screen-xl px-4 py-2 sm:px-6 lg:px-8">
+            <div className="hero mx-auto max-w-screen-xl px-4 py-0 md:py-2 sm:px-6 lg:px-8">
                 <div className="lg:flex lg:items-start lg:gap-8">
                     <div className="text-center">
                         <div className="inline-flex">
@@ -37,14 +37,13 @@ export default function Subscribe(){
                                 </h2>
                             </div>
                         </div>
-
-                        <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end">
+                        <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end mb-5 md:mb-0">
                             <form onSubmit={submitForm} ref={formRef} className="w-full">
                                 <label htmlFor="UserEmail" className="sr-only"> 
                                     Email 
                                 </label>
 
-                                <div className="p-2 sm:flex sm:items-center">
+                                <div className="p-0 md:p-2 sm:flex sm:items-center">
                                     <div className="sm:flex w-full lg:ring ring-secondary focus-within:ring-primary rounded-full">
                                         <input
                                             type="email"
@@ -53,7 +52,6 @@ export default function Subscribe(){
                                             placeholder="john@example.com"
                                             className="w-full border-none rounded-full sm:rounded-none sm:rounded-l-full focus:border-transparent focus:ring-transparent sm:text-sm py-3 px-5"
                                         />
-
                                         <button
                                             type="submit"
                                             className="mt-1 w-full rounded-full sm:rounded-none sm:rounded-r-full bg-primary hover:bg-secondary group px-6 py-3 text-sm font-bold uppercase tracking-wide text-white transition-none sm:mt-0 sm:w-auto sm:shrink-0">
