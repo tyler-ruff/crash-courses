fee5438390e259100f14175d7b3993ea06e870e1
Tyler Ruff <truff@blazed.work>
2023-11-25 03:06:33 -0500

    Update data.ts

--- components/gallery/data.ts
+++ components/gallery/data.ts
@@ -1,7 +1,7 @@
 export const galleryData = [
-    "/images/gallery/gallery-3.jpg",
+    "/images/gallery/gallery-3.jpeg",
     "/images/gallery/gallery-4.jpg",
-    "/images/gallery/gallery-2.jpeg",
+    "/images/gallery/gallery-2.jpg",
     "/images/gallery/gallery-1.jpg"
 ];
 
